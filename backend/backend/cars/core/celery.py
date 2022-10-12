b3378987732c57ef2cdf8ae12081e6dfdb28a21c
Tarlex <tarlex.py@ya.ru>
2022-10-12 17:02:50 +0300

    tasks

--- backend/backend/cars/core/celery.py
+++ backend/backend/cars/core/celery.py
@@ -1,7 +1,27 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 app = Celery("cars")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "take-date-from-integration": {
+        "task": "tasks.take_date_from_integration",
+        "schedule": crontab(hour="*/4"),
+        "args": ("http://export.maxposter.ru/legocar-used-all/3073-15554.xml",),
+    },
+    "take-date-from-integration": {
+        "task": "tasks.take_date_from_integration",
+        "schedule": crontab(hour="*/4"),
+        "args": ("http://export.maxposter.ru/legocar-used-all/3413-78456.xml",),
+    },
+    "take-date-from-integration": {
+        "task": "tasks.take_date_from_integration",
+        "schedule": crontab(hour="*/4"),
+        # Function Arguments
+        "args": ("http://kan-ftp.kanavto.ru/newavto/Avtoru_kae_renaultoren.xml",),
+    },
+}
--- backend/backend/cars/core/settings.py
+++ backend/backend/cars/core/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "django_filters",
     "django_crontab",
+    "django_celery_beat",
     # apps
     "main",
     "gallery",
@@ -100,8 +101,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
